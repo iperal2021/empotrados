61e23aab2b6631dda8f2c779e6176ee517615e16
Iker Peral del Pino <i.peral.2021@alumnos.urjc.es>
2023-11-23 10:13:55 +0100

    avances

--- maquina_expendedora/maquina_expendedora.ino
+++ maquina_expendedora/maquina_expendedora.ino
@@ -25,8 +25,15 @@ int distance;
 int X_axix;
 int Y_axix;
 
+int STATE = 0;
+float price_list[5] = {1.00, 1.10, 1.25, 1.50, 2.00};
+String cafe_list[5] = {"CAFE SOLO", "CAFE CORTADO", "CAFE DOBLE", "CAFE PREMIUM", "CHOCOLATE"};
+
+int randNumber;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   lcd.begin(16,2);
 
   pinMode(TRIGGER, OUTPUT);
@@ -35,6 +42,7 @@ void setup() {
   pinMode(LED_RED,OUTPUT);
   pinMode(LED_GREEN,OUTPUT);
   pinMode(BUTTON, INPUT);
+  pinMode(JS_DIGITAL, INPUT_PULLUP);
 
   lcd.print("Cargando...");
   for (int i = 0; i < 3; i++){
@@ -43,6 +51,14 @@ void setup() {
     analogWrite(LED_RED,0);
     delay(1000);
   }
+}
+
+void cafe_menu() {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(cafe_list[STATE]);
+  lcd.setCursor(0,1);
+  lcd.print(price_list[STATE]);
 }
 
 void loop() {
@@ -59,7 +75,32 @@ void loop() {
   Y_axix = analogRead(JS_Y);
 
   lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Servicio");
+
+  if(X_axix > 500){
+    STATE +=1;
+    if (STATE == 5){
+      STATE = 0;
+    }
+  }else if (X_axix < 480){
+    STATE -=1;
+    if (STATE == -1){
+      STATE = 4;
+    }
+  }
+
+  if (digitalRead(JS_DIGITAL) == LOW){
+    lcd.clear();
+    randNumber = random(4, 8);
+    lcd.print("Sirviendo...");
+    for (int i = 1; i< 255; i++){
+      analogWrite(LED_GREEN, i);
+      delay(randNumber*1000/253);
+    }
+    analogWrite(LED_GREEN, 0);
+    lcd.clear();
+    lcd.print("RETIRE BEBIDA");
+    delay(3000);
+  }
+  cafe_menu();
 
 }
